f3242a9a89132185d4492e1f15773a6a1a284c70
Lei Ding <dinn@duedex.com>
2022-05-26 13:56:33 +0800

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
 import { Address } from '@graphprotocol/graph-ts'
 import { MetaN1 } from './types/MetaN1/MetaN1'
 
-export const contract = MetaN1.bind(Address.fromString('0x2507d76A58c7be37E9dbBBAb067673203ED62318'))
+export const contract = MetaN1.bind(Address.fromString('0xCA668423376Ee23ED40746275A79f626b24e9DAF'))
